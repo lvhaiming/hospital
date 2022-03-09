df14967239b3e4798d15d8294832389cb5100296
lvhaiming <lvhaiming@f-road.com.cn>
2022-03-09 17:08:45 +0800

    axios请求添加请求进度

--- hospitalFront/src/lib/axios.js
+++ hospitalFront/src/lib/axios.js
@@ -2,10 +2,21 @@ import Axios from 'axios'
 import Vue from 'vue'
 
 Axios.defaults.baseURL = 'http://localhost:3000'
+// 添加请求拦截器
+Axios.interceptors.request.use(config => {
+    Vue.prototype.$Loading.start()
+    return config
+}, error => {
+    // 对请求错误做些什么
+    return Promise.reject(error)
+})
+
 // 添加响应拦截器
 Axios.interceptors.response.use(response => {
+    Vue.prototype.$Loading.finish()
     return response;
 }, error => {
+    Vue.prototype.$Loading.finish()
     Vue.prototype.$Message.error('请求失败，请联系管理员')
     return error
 })
