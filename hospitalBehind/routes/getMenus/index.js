bb82eb73934ce36c3c77402fb087c8256f88cd1f
lvhaiming <lvhaiming@f-road.com.cn>
2022-03-17 12:05:33 +0800

    修改菜单获取逻辑

--- hospitalBehind/routes/getMenus/index.js
+++ hospitalBehind/routes/getMenus/index.js
@@ -8,40 +8,45 @@ class User {
     // 获取菜单列表
     getMenus(req, res) {
         let professional = req.body.professional // 获取参数
-        this.connection.query(`select * from menu;`, (err, result) => {
-            if (err) {
-                console.log(err)
-            } else {
-                console.log('result :>> ', result);
-                let data = []
-                result.forEach(item => {
-                    if (item.parentId) {
-                        data.push(item)
-                    }
-                })
-                // 整理菜单列表
-                data.map(item => {
-                    item.children = []
-                    result.forEach(list => {
-                        if (list.parent) {
-                            if (list.parent == item.parentId) {
-                                item.children.push(list)
+        let p = new Promise((resolve, reject) => {
+            this.connection.query(`select * from professional where professional_id='${professional}';`, (err, result) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    resolve(result[0].professional_auth.split(',')) // promise返回professional_auth处理后的数组
+                }
+            })
+        })
+        p.then(result => {
+            this.connection.query(`select * from menu;`, (err, data) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    let arr = []
+                    result.forEach(item => {
+                        data.forEach(list => {
+                            if (item == list.id && list.parentId) {
+                                arr.push(list) // arr接收有父级字段的菜单，并且菜单id存在result数组里面
                             }
-                        }
+                        })
                     })
-                })
-                let resultData = []
-                data.forEach(item => {
-                    if (item.auth.split(",").indexOf(professional) > -1) {
-                        resultData.push(item)
-                    }
-                })
-                res.send({
-                    data: resultData,
-                    code: '0000',
-                    msg: 'ok'
-                })
-            }
+                    arr.forEach(item => {
+                        item.children = []
+                        data.forEach(list => {
+                            if (list.parent) {
+                                if (list.parent == item.parentId) {
+                                    item.children.push(list) // 给父级菜单添加子菜单
+                                }
+                            }
+                        })
+                    })
+                    res.send({
+                        data: arr,
+                        code: '0000',
+                        msg: 'ok'
+                    })
+                }
+            })
         })
     }
 }
